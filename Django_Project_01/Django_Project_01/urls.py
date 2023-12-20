2d8a44bd408c6dcc8911ae008a07fc60c05bde97
PI-Klimkovich <pol-ik@mail.ru>
2023-12-20 17:13:42 +0300

    Созданы "черновики" главной страницы и страницы о приложении

--- Django_Project_01/Django_Project_01/urls.py
+++ Django_Project_01/Django_Project_01/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from notes.views import home_page_view, about_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page_view),
+    path('about', about_view),
 ]
